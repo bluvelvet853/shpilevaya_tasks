3074dc5057e410f29a160c37214753a86a1f216d
bluevelvet853 <bluevelvet853853@gmail.com>
2017-03-01 10:18:09 +0500

    first commit

---
+++ ticket/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ticket = 234647;
+            Console.WriteLine(ticket);
+            Console.ReadKey();
+        }
+    }
+}
