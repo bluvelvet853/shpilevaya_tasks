12edda68dc5ed121c100b31565f1b902cb04d048
bluevelvet853 <bluevelvet853853@gmail.com>
2017-04-26 10:44:43 +0500

    dasdas

---
+++ dlina/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dlina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\student\Desktop\kolobok.txt");
+            Console.WriteLine(text);
+
+            //удаляем пробелы в конце и начале строки (если они есть)
+            text = text.Trim();
+
+            //заносим в массив Arr все слова, разделённые пробелом
+            string[] Arr = text.Split(' ');
+
+            int min = 999999;
+            int max = 0;
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if (Arr[i].Length < min) min = Arr[i].Length;
+                if (Arr[i].Length > max) max = Arr[i].Length;
+            }
+
+            Console.WriteLine("Самое длинное слово: {0}\nСамое короткое слово: {1}", max, min);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ polindrom/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\student\Desktop\kolobok.txt");
+            // считываем текст
+            Console.WriteLine("Текст: " + text);
+            // выводим текст
+            string smallText = text.ToLower(); //переводим текст в нижний регистр
+            string slova = smallText.Trim(new char[] { ',', '.', '—', '«', '»', '-' });
+            string[] textSlova = slova.Split(new char[] { ',', '.', ' ', '—', '«', '»', '-' });
+            // исключаем символы из текста
+            string palindrom = null;
+            for (int i = 0; i < textSlova.Length; i++)
+            {
+                string slovo = textSlova[i];
+                int t = 0;
+                if (textSlova[i].Trim() != "")
+                {
+                    if (slovo.Length != 1)
+                    {
+                        if (slovo.Length % 2 == 0)
+                        {
+                            for (int a = 0; a < (slovo.Length / 2); a++)
+                            {
+                                char bukva1 = slovo[a];
+                                int b = slovo.Length - 1;
+
+                                char bukva2 = slovo[b - a];
+                                if (bukva1 == bukva2)
+                                {
+                                    t = t + 1;
+                                }
+                            }
+
+                            if (t == slovo.Length / 2)
+                            {
+                                palindrom = palindrom + slovo + " ";
+                            }
+                        }
+                        else
+                        {
+                            for (int a = 0; a < ((slovo.Length - 1) / 2); a++)
+                            {
+                                char bukva1 = slovo[a];
+                                int b = slovo.Length - 1;
+
+                                char bukva2 = slovo[b - a];
+                                if (bukva1 == bukva2)
+                                {
+                                    t = t + 1;
+                                }
+                            }
+                            if (t == slovo.Length / 2)
+                            {
+                                palindrom = slovo + " ";
+                                Console.WriteLine("Палиндромом является слово - " + palindrom);
+                                // вывод палиндрома
+                            }                            
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
