6f948fbfaa1f52c04f9b4197240acf483322f79a
bluevelvet853 <bluevelvet853853@gmail.com>
2017-05-17 10:59:34 +0500

    DVUMMAS17.05

---
+++ DvMas/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvMas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArr = new int[5, 6];
+            Random ran = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    myArr[i, j] = ran.Next(1, 10);
+                    Console.Write("{0}\t", myArr[i, j]);
+                }
+            }
+            int st = 0;
+            for (int j = 0; j < 6; j++)
+            {
+                int srar = 0;
+                int itog = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    srar += myArr[i, j];
+                    itog = srar / 5;
+                }
+                st = st + 1;
+                Console.WriteLine("Среднее арифметическое {0} столбца = {1}", st, itog);
+            }
+
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                max = 0;
+                min = 0;
+                for (int j = 0; j < 6; j++)
+                {
+                    int ch = j;
+                    max = ch;
+                    if (ch >= max)
+                    {
+                        max = ch;
+                    }
+                    else
+                        min = ch;
+                }
+                Console.WriteLine(max);
+                Console.WriteLine(min);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DvMas19/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvMas19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArr = new int[7, 7];
+            Random ran = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    myArr[i, j] = ran.Next(1, 10);
+                    Console.Write("{0}\t", myArr[i, j]);
+                }
+            }
+            int str = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                int summa = 0;
+                for (int j = 0; j < 7; j++)
+                {
+                    summa += myArr[i, j];
+                }
+                str = str + 1;
+                Console.WriteLine("\r\n Сумма {0} строки = {1}", str, summa);
+            }
+            int summa2 = 0;
+            int max = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                int summa1 = 0;
+                for (int j = 0; j < 7; j++)
+                {
+                    summa1 += myArr[i, j];
+                    if (summa2 >= summa1)
+                    {
+                        max = summa2;
+                    }
+                    else
+                    {
+                        max = summa1;
+                    }
+                }
+                summa2 = summa1;
+            }
+            Console.WriteLine("\r\n Наибольшая сумма элементов равна " + max);
+            Console.ReadKey();
+        }
+    }
+}
